779d224648ae0e601aeb628e32fd32d6ceafd51e
James Garzón <james.garzon@udea.edu.co>
2016-02-27 19:25:14 -0500

    modelo de datos

--- server/api/estudiante/estudiante.model.js
+++ server/api/estudiante/estudiante.model.js
@@ -3,14 +3,102 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var EstudianteSchema = new mongoose.Schema({
-  identificacion: String,
-  nombre: String,
-  apellido: String,
-  fecha_nacimiento: Date,
-  ciudad: String,
-  direccion: String,
-  telefono: String,
-  email: String
+  tipoDocumento : String,
+  documento : String,
+  nombres : String,
+  apellidos : String,
+  direccion : String,
+  telefonos : [ { tipo :String, titular : String, numero : String } ],
+  email : String,
+  acudiente : String, //Cambiar cuando creemos la entidad
+  periodos : [
+    {
+      codigo : String, // 2016-1
+      grupo : String,
+      areas : [
+        {
+          nombre : String,
+          nota : Number,
+          notaEscolar : String,
+          notaNacional : String,
+          asignaturas : [
+            {
+              nombre : String,
+              docente : String, // Cambiar cuando creemos la entidad
+              asistencias : [Date],
+              actividades : [
+                {
+                titulo : String,
+                descripcion : String,
+                fechaCreacion : Date,
+                nota : Number,
+                notaEscolar : String,
+                notaNacional : String,
+                desempeno : {
+                  nombre : String,
+                  descripcion : String,
+                  tipo : [String]
+                }
+              }
+              ]
+            }
+          ]
+        }
+      ]
+      }
+    ]
+
+
+
 });
 
 export default mongoose.model('Estudiante', EstudianteSchema);
+
+// {
+//   "tipoDocumento" : "CC",
+//   "documento" : "1040040896",
+//   "nombres" : "JAMES DANILO",
+//   "apellidos" : "GARZÓN OTALVARO",
+//   "direccion" : "CALLE 16 #56678",
+//   "telefonos" : [ { "tipo" :"HOGAR", "titular" : "CENELLY", "numero" : "3457765345" } ],
+//   "email" : "jamesgarzon@gmail.com,
+//   "acudiente" : "CENELLY",
+//   "periodos" : [
+//     {
+//       "codigo" : "2016-1", // 2016-1
+//       "grupo" : "9B",
+//       "areas" : [
+//         {
+//           "nombre" : "HUMANIDADES-9",
+//           "nota" : 5,
+//           "notaEscolar" : "E",
+//           "notaNacional" : "S",
+//           "asignaturas" : [
+//             {
+//               "nombre" : "ESPAÑOL",
+//               "docente" : "9201930564",
+//               "asistencias" : ["2015-01-09","2015-01-10"],
+//               "actividades" : [
+//                 {
+//                 "titulo" : "ENSAYO EL PRINCIPITO",
+//                 "descripcion" : "Obviamente es un ensayo sobre el libro de el principito",
+//                 "fechaCreacion" : "2016-02-20",
+//                 "nota" : 5,
+//                 "notaEscolar" : "E",
+//                 "notaNacional" : "S",
+//                 "desempeno" : {
+//                   "nombre" : "SER",
+//                   "descripcion" : "Soy tu padre",
+//                   "tipo" : ["SER"]
+//                 }
+//               }
+//               ]
+//             }
+//           ]
+//         }
+//       ]
+//       }
+//     ]
+// }
+
+// var mongoose = require('mongoose'); var ObjectId = mongoose.Schema.Types.ObjectId; var Car = new Schema({ driver: ObjectId }); // or just Schema.ObjectId for backwards compatibility with v2
--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,8 +6,8 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/notas-dev'
-    // uri: 'mongodb://james:dementor@ds035735.mongolab.com:35735/notas',
+    // uri: 'mongodb://localhost/notas-dev'
+    uri: 'mongodb://james:dementor@ds035735.mongolab.com:35735/notas',
     // uri: 'mongodb://esteban:boneserdio@ds055535.mongolab.com:55535/desempenos'
   },
 
