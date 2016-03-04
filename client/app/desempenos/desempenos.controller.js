1b937a33cb3a8132ae5ab23d6aa4cb680766ffd2
Boamorte8 <salazar.z.esteban@gmail.com>
2016-03-04 17:47:39 -0500

    Servicios listar y crear desempeños listos

--- client/app/desempenos/desempenos.controller.js
+++ client/app/desempenos/desempenos.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('notasApp')
-  .controller('DesempenosCtrl', function ($scope,Desempenos) {
+  .controller('DesempenosCtrl', function ($scope,Desempeno) {
     $(document).ready(function(){
       $('.collapsible').collapsible({
         accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
@@ -17,7 +17,30 @@ angular.module('notasApp')
       "Ser",
       "Estar",
       "Hacer"
-    ]
+    ];
 
+    $scope.listarDesempenos = function () {
+      Desempeno.listar()
+      .then(function(data) {
+            $scope.desempenos = data;
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+    }
 
+    $scope.crearDesempeno = function (desempeno) {
+      Desempeno.crear(desempeno)
+      .then(function(data) {
+        $('#modal-desempeno-form').closeModal();
+        $scope.listarDesempenos();
+        Materialize.toast('Desempeño creado con éxito', 4000) // 4000 is the duration of the toast
+      })
+      .catch(function(err) {
+        Materialize.toast('Hubo un error creando el Desempeño', 1000) // 4000 is the duration of the toast
+        console.log(err);
+      });
+    }
+
+    $scope.listarDesempenos();
   });
