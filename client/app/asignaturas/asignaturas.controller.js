fac720ec0891c56e3a5b13121d50e3f3109e4f0d
James <james.garzon@udea.edu.co>
2016-03-04 17:46:58 -0500

    funcionalidades al controlador de notas

--- client/app/asignaturas/asignaturas.controller.js
+++ client/app/asignaturas/asignaturas.controller.js
@@ -1,36 +1,7 @@
 'use strict';
 
 angular.module('notasApp')
-  .controller('AsignaturasCtrl', function ($scope, Estudiante, Actividad) {
-
-    $scope.listarEstudiantes = function () {
-      Estudiante.listar()
-      .then(function(data) {
-        $scope.estudiantes = data;
-        // for (var i = 0; i < $scope.estudiantes.length; i++) {
-        //   $scope.estudiantes[i].fecha_nacimiento = $filter('date')($scope.estudiantes[i].fecha_nacimiento, "yyyy-MM-dd");
-        // }
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    };
-
-    $scope.listarActividades = function () {
-      Actividad.listar()
-      .then(function(data) {
-        $scope.actividades = data;
-        // for (var i = 0; i < $scope.estudiantes.length; i++) {
-        //   $scope.estudiantes[i].fecha_nacimiento = $filter('date')($scope.estudiantes[i].fecha_nacimiento, "yyyy-MM-dd");
-        // }
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    };
-
-    $scope.listarActividades();
-    $scope.listarEstudiantes();
+  .controller('AsignaturasCtrl', function ($scope) {
 
 
   });
--- client/app/notas/notas.controller.js
+++ client/app/notas/notas.controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('notasApp')
-  .controller('NotasCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('NotasCtrl', function ($scope,Estudiante) {
+
+        $scope.listarEstudiantes = function () {
+          Estudiante.listar()
+          .then(function(data) {
+            $scope.estudiantes = data;
+            // for (var i = 0; i < $scope.estudiantes.length; i++) {
+            //   $scope.estudiantes[i].fecha_nacimiento = $filter('date')($scope.estudiantes[i].fecha_nacimiento, "yyyy-MM-dd");
+            // }
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+        };
+
+
+        $scope.listarEstudiantes();
+
   });
