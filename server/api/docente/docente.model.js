fe568daeb51ca5d28a85ac4cfa645d374b0c6bb0
camilo-guerra <camilo.guerra.gonzalez@gmail.com>
2016-05-31 10:51:07 -0500

    cambios en el modelo docente

--- server/api/docente/docente.model.js
+++ server/api/docente/docente.model.js
@@ -5,8 +5,12 @@ import mongoose from 'mongoose';
 var DocenteSchema = new mongoose.Schema({
   user_id:String;
   asignaturas:[{
+  /* Codigo de periodo para saber en que periodos dio que materias*/codigo:String,
   	nombre: String,
-  	grupo:String
+  	grupo:String,
+  	actividades:[{
+  		id_actividad:String
+  	}]
   }]
 });
 
